d9e7179e1e0427e8efb723472fd1d6233a2231a3
prtk1729 <prateek.pani4243@gmail.com>
2024-10-20 18:19:00 +0530

    Completed and verified Paligemma Processor and added \n as per the referncee above

--- processing_paligemma.py
+++ processing_paligemma.py
@@ -74,7 +74,14 @@ def create_gemma_string(prefix_prompt,
                         image_token,
                         bos_token
                         ):
-    return f"{image_token * image_seq_len}{bos_token}{prefix_prompt}"
+    # Quoting from the blog (https://huggingface.co/blog/paligemma#detailed-inference-process):
+    #   The input text is tokenized normally. 
+    #   A <bos> token is added at the beginning, and an additional newline token (\n) is appended.
+    #   This newline token is an essential part of the input prompt the model was trained with, so adding it explicitly ensures it's always there.
+    #   The tokenized text is also prefixed with a fixed number of <image> tokens.
+    # NOTE: from the paper it looks like the `\n` should be tokenized separately, but in the HF implementation this is not done.
+    #       ref to HF implementation: https://github.com/huggingface/transformers/blob/7f79a97399bb52aad8460e1da2f36577d5dccfed/src/transformers/models/paligemma/processing_paligemma.py#L55-L73
+    return f"{image_token * image_seq_len}{bos_token}{prefix_prompt}\n"
     
 
 
